9f0cdc93e35044bd40afe6966067e6b4984c1c83
DJUNDERSCOREWOLF <99215368+DJUNDERSCOREWOLF@users.noreply.github.com>
2022-11-16 11:35:05 -0700

    New Project
    New Project Created

---
+++ A Tale of Two Gamerz/Assets/Scripts/Teleport.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+
+    float distance = 500;
+    public GameObject Trapdoor;
+    void OnTriggerEnter2D(Collider2D Trapdoor)
+    {
+        transform.position = new Vector2(transform.position.x + distance, transform.position.y);
+    }
+}
