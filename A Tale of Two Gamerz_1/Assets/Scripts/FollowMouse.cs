b508f3e8402d945a2982d3d056df140987208f68
Kingdondon101 <william.cracroft@sastudent.com>
2022-11-23 21:17:36 -0700

    Attack Anim
    Added the attack animation. (Might need to resize)

--- A Tale of Two Gamerz_1/Assets/Scripts/FollowMouse.cs
+++ A Tale of Two Gamerz_1/Assets/Scripts/FollowMouse.cs
@@ -7,29 +7,36 @@ public class FollowMouse : MonoBehaviour
     public Transform Knife;
     public float radius;
     public Transform Player;
-    public float Thrust = 20f;
+    public bool Attacking;
     private Transform pivot;
-
-    
+    public Animator animator;
 
     void Start()
     {
+        //Vector3 local = transform.localScale;
+
         pivot = Knife.transform;
         transform.parent = pivot;
-        transform.position += Vector3.up * radius;
+        //transform.position += Vector3.up * radius;
     }
 
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            transform.position = new Vector2(transform.position.x,transform.position.y / Thrust);
+            animator.SetBool("Attacking", true);
+            Attacking = true;
+            //transform.localScale = new Vector3(20, 20, 0);
         }
         else
         {
             Vector3 KnifeVector = Camera.main.WorldToScreenPoint(Knife.position);
             KnifeVector = Input.mousePosition - KnifeVector;
             float angle = Mathf.Atan2(KnifeVector.y, KnifeVector.x) * Mathf.Rad2Deg;
+
+            animator.SetBool("Attacking", false);
+            Attacking = false;
+            //transform.localScale = new Vector3(1, 1, 0);
 
             pivot.position = Player.position;
             pivot.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
